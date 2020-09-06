6410e791160a4e2d4dff75d97f0ee2dd054423ba
Ivan Suvorov <avatar2329@gmail.com>
2020-09-06 12:19:51 +0600

    [THS] Update SSE Files.

--- sp/game/ths_iv_main/shadereditorui/shader_src/common_ps_fxc.h
+++ sp/game/ths_iv_main/shadereditorui/shader_src/common_ps_fxc.h
@@ -1,4 +1,4 @@
-//====== Copyright © 1996-2007, Valve Corporation, All rights reserved. =======//
+//========= Copyright Valve Corporation, All rights reserved. ============//
 //
 // Purpose: Common pixel shader code
 //
@@ -43,7 +43,6 @@ const bool g_bHighQualityShadows : register( b0 );
 
 // NOTE: w == 1.0f / (Dest alpha compressed depth range).
 const float4 g_LinearFogColor : register( c29 );
-
 #define OO_DESTALPHA_DEPTH_RANGE (g_LinearFogColor.w)
 
 // Linear and gamma light scale values
@@ -55,8 +54,8 @@ const float4 cLightScale : register( c30 );
 
 // Flashlight constants
 #if defined(SHADER_MODEL_PS_2_0) || defined(SHADER_MODEL_PS_2_B) || defined(SHADER_MODEL_PS_3_0)
- const float4 cFlashlightColor       : register( c10 ); //register( c28 );
- const float4 cFlashlightScreenScale : register( c11 ); //register( c31 ); // .zw are currently unused
+ const float4 cFlashlightColor       : register( c28 );
+ const float4 cFlashlightScreenScale : register( c31 ); // .zw are currently unused
  #define flFlashlightNoLambertValue cFlashlightColor.w // This is either 0.0 or 2.0
 #endif
 
@@ -193,7 +192,7 @@ float4 DecompressNormal( sampler NormalSampler, float2 tc, int nDecompressionMod
 HALF3 NormalizeWithCubemap( sampler normalizeSampler, HALF3 input )
 {
 //	return texCUBE( normalizeSampler, input ) * 2.0f - 1.0f;
-	return texCUBE( normalizeSampler, input );
+	return texCUBE( normalizeSampler, input ).xyz;
 }
 
 /*
@@ -210,8 +209,17 @@ HALF4 EnvReflect( sampler envmapSampler,
 }
 */
 
+// Vectorized smoothstep for doing three smoothsteps at once.  Used by uberlight
+float3 smoothstep3( float3 edge0, float3 edge1, float3 OneOverWidth, float3 x )
+{
+	x = saturate((x - edge0) * OneOverWidth);	// Scale, bias and saturate x to the range of zero to one
+	return x*x*(3-2*x);							// Evaluate polynomial
+}
+
 float CalcWaterFogAlpha( const float flWaterZ, const float flEyePosZ, const float flWorldPosZ, const float flProjPosZ, const float flFogOORange )
 {
+#if 0
+	// This version is what you use if you want a line-integral throught he water for water fog.
 //	float flDepthFromWater = flWaterZ - flWorldPosZ + 2.0f; // hackity hack . .this is for the DF_FUDGE_UP in view_scene.cpp
 	float flDepthFromWater = flWaterZ - flWorldPosZ;
 
@@ -223,57 +231,46 @@ float CalcWaterFogAlpha( const float flWaterZ, const float flEyePosZ, const floa
 	// Calculate the ratio of water fog to regular fog (ie. how much of the distance from the viewer
 	// to the vert is actually underwater.
 	float flDepthFromEye = flEyePosZ - flWorldPosZ;
-	float f = (flDepthFromWater / flDepthFromEye) * flProjPosZ;
+	float f = saturate(flDepthFromWater * (1.0/flDepthFromEye));
 
 	// $tmp.w is now the distance that we see through water.
-	return saturate( f * flFogOORange );
+	return saturate(f * flProjPosZ * flFogOORange);
+#else
+	// This version is simply using the depth of the water to determine fog factor,
+	// which is cheaper than doing the line integral and also fixes some problems with having 
+	// a hard line on the shore when the water surface is viewed tangentially.
+	// hackity hack . .the 2.0 is for the DF_FUDGE_UP in view_scene.cpp
+	return saturate( ( flWaterZ - flWorldPosZ - 2.0f ) * flFogOORange );
+#endif
 }
 
-#if defined(SHADER_EDITOR_SWARM_COMPILE)
-float CalcRangeFog( const float3 flEyePos, const float3 flWorldPos, const float flFogEndOverRange, const float flFogMaxDensity, const float flFogOORange )
-#else
-float CalcRangeFog( const float flProjPosZ, const float flFogEndOverRange, const float flFogMaxDensity, const float flFogOORange )
-#endif // SHADER_EDITOR_SWARM_COMPILE
+float CalcRangeFog( const float flProjPosZ, const float flFogStartOverRange, const float flFogMaxDensity, const float flFogOORange )
 {
-#if defined(SHADER_EDITOR_SWARM_COMPILE)
-	return min( flFogMaxDensity, saturate( flFogEndOverRange + ( distance( flEyePos, flWorldPos ) * flFogOORange ) ) );
-#elif defined(SHADER_EDITOR_2013_COMPILE)
-	return min( flFogMaxDensity, ( saturate( flProjPosZ * flFogOORange - flFogEndOverRange ) ) );
-#else
 #if !(defined(SHADER_MODEL_PS_1_1) || defined(SHADER_MODEL_PS_1_4) || defined(SHADER_MODEL_PS_2_0)) //Minimum requirement of ps2b
-	return min( flFogMaxDensity, ( saturate( 1.0 - (flFogEndOverRange - (flProjPosZ * flFogOORange)) ) ) );
+	return saturate( min( flFogMaxDensity, (flProjPosZ * flFogOORange) - flFogStartOverRange ) );
 #else
 	return 0.0f; //ps20 shaders will never have range fog enabled because too many ran out of slots.
 #endif
-#endif // SHADER_EDITOR_SWARM_COMPILE
 }
 
-#if defined(SHADER_EDITOR_SWARM_COMPILE)
-float CalcPixelFogFactor( int iPIXELFOGTYPE, const float4 fogParams, const float3 flEyePos, const float3 flWorldPos )
-#else
-float CalcPixelFogFactor( int iPIXELFOGTYPE, const float4 fogParams, const float flEyePosZ, const float flWorldPosZ, const float flProjPosZ )
-#endif // SHADER_EDITOR_SWARM_COMPILE
+float CalcPixelFogFactor( int iPIXELFOGTYPE, const float4 fogParams, const float3 vEyePos, const float3 vWorldPos, const float flProjPosZ )
 {
-	float retVal;
-	if ( iPIXELFOGTYPE == PIXEL_FOG_TYPE_NONE )
+	float retVal = 0;
+	/*if ( iPIXELFOGTYPE == PIXEL_FOG_TYPE_NONE )
 	{
 		retVal = 0.0f;
-	}
+	}*/
 	if ( iPIXELFOGTYPE == PIXEL_FOG_TYPE_RANGE ) //range fog, or no fog depending on fog parameters
 	{
-#if defined(SHADER_EDITOR_SWARM_COMPILE)
-		retVal = CalcRangeFog( flEyePos, flWorldPos, fogParams.x, fogParams.z, fogParams.w );
-#else
-		retVal = CalcRangeFog( flProjPosZ, fogParams.x, fogParams.z, fogParams.w );
-#endif // SHADER_EDITOR_SWARM_COMPILE
+		//retVal = CalcRangeFog( flProjPosZ, fogParams.x, fogParams.z, fogParams.w );
+		float flFogMaxDensity = fogParams.z;
+		float flFogEndOverRange = fogParams.x;
+		float flFogOORange = fogParams.w;
+		retVal = CalcRangeFogFactorNonFixedFunction( vWorldPos, vEyePos, flFogMaxDensity, flFogEndOverRange, flFogOORange );
 	}
 	else if ( iPIXELFOGTYPE == PIXEL_FOG_TYPE_HEIGHT ) //height fog
 	{
-#if defined(SHADER_EDITOR_SWARM_COMPILE)
-		retVal = 0.0f;
-#else
-		retVal = CalcWaterFogAlpha( fogParams.y, flEyePosZ, flWorldPosZ, flProjPosZ, fogParams.w );
-#endif // SHADER_EDITOR_SWARM_COMPILE
+		retVal = CalcWaterFogAlpha( fogParams.y, vEyePos.z, vWorldPos.z, flProjPosZ, fogParams.w );
 	}
 
 	return retVal;
@@ -287,23 +284,25 @@ float CalcPixelFogFactor( int iPIXELFOGTYPE, const float4 fogParams, const float
 
 float3 BlendPixelFog( const float3 vShaderColor, float pixelFogFactor, const float3 vFogColor, const int iPIXELFOGTYPE )
 {
+	float3 flRet = 0;
 	if( iPIXELFOGTYPE == PIXEL_FOG_TYPE_RANGE ) //either range fog or no fog depending on fog parameters and whether this is ps20 or ps2b
 	{
 #	if !(defined(SHADER_MODEL_PS_1_1) || defined(SHADER_MODEL_PS_1_4) || defined(SHADER_MODEL_PS_2_0)) //Minimum requirement of ps2b
 		pixelFogFactor = saturate( pixelFogFactor );
-		return lerp( vShaderColor.rgb, vFogColor.rgb, pixelFogFactor * pixelFogFactor ); //squaring the factor will get the middle range mixing closer to hardware fog
+		flRet = lerp( vShaderColor.rgb, vFogColor.rgb, pixelFogFactor * pixelFogFactor ); //squaring the factor will get the middle range mixing closer to hardware fog
 #	else
-		return vShaderColor;
+		flRet = vShaderColor;
 #	endif
 	}
 	else if( iPIXELFOGTYPE == PIXEL_FOG_TYPE_HEIGHT )
 	{
-		return lerp( vShaderColor.rgb, vFogColor.rgb, saturate( pixelFogFactor ) );
+		flRet = lerp( vShaderColor.rgb, vFogColor.rgb, saturate( pixelFogFactor ) );
 	}
 	else if( iPIXELFOGTYPE == PIXEL_FOG_TYPE_NONE )
 	{
-		return vShaderColor;
+		flRet = vShaderColor;
 	}
+	return flRet;
 }
 
 
@@ -363,23 +362,15 @@ float4 FinalOutput( const float4 vShaderColor, float pixelFogFactor, const int i
 	}
 	
 	if( bWriteDepthToDestAlpha )
-#if ( WRITEWATERFOGTODESTALPHA == 1 )
-		result.a = pixelFogFactor;
-#else
 		result.a = DepthToDestAlpha( flProjZ );
-#endif
 	else
 		result.a = vShaderColor.a;
 
 	result.rgb = BlendPixelFog( result.rgb, pixelFogFactor, g_LinearFogColor.rgb, iPIXELFOGTYPE );
-
-#if !(defined(SHADER_EDITOR_SWARM_COMPILE))
-
+	
 #if !(defined(SHADER_MODEL_PS_1_1) || defined(SHADER_MODEL_PS_1_4) || defined(SHADER_MODEL_PS_2_0)) //Minimum requirement of ps2b
 	result.rgb = SRGBOutput( result.rgb ); //SRGB in pixel shader conversion
 #endif
-
-#endif // SHADER_EDITOR_SWARM_COMPILE
 
 	return result;
 }
@@ -410,6 +401,172 @@ float RemapValClamped( float val, float A, float B, float C, float D)
 
 	return C + (D - C) * cVal;
 }
+
+
+//===================================================================================//
+// This is based on Natasha Tatarchuk's Parallax Occlusion Mapping (ATI)
+//===================================================================================//
+// INPUT:
+//		inTexCoord: 
+//			the texcoord for the height/displacement map before parallaxing
+//
+//		vParallax:
+//			Compute initial parallax displacement direction:
+//			float2 vParallaxDirection = normalize( vViewTS.xy );
+//			float fLength = length( vViewTS );
+//			float fParallaxLength = sqrt( fLength * fLength - vViewTS.z * vViewTS.z ) / vViewTS.z; 
+//			Out.vParallax = vParallaxDirection * fParallaxLength * fProjectedBumpHeight;
+//
+//		vNormal:
+//			tangent space normal
+//
+//		vViewW: 
+//			float3 vViewW = /*normalize*/(mul( matViewInverse, float4( 0, 0, 0, 1)) - inPosition );
+//
+// OUTPUT:
+//		the new texcoord after parallaxing
+float2 CalcParallaxedTexCoord( float2 inTexCoord, float2 vParallax, float3 vNormal, 
+							   float3 vViewW, sampler HeightMapSampler )
+{
+	const int nMinSamples = 8;
+	const int nMaxSamples = 50;
+
+   //  Normalize the incoming view vector to avoid artifacts:
+//   vView = normalize( vView );
+   vViewW = normalize( vViewW );
+//   vLight = normalize( vLight );
+   
+   // Change the number of samples per ray depending on the viewing angle
+   // for the surface. Oblique angles require smaller step sizes to achieve 
+   // more accurate precision         
+   int nNumSteps = (int) lerp( nMaxSamples, nMinSamples, dot( vViewW, vNormal ) );
+      
+   float4 cResultColor = float4( 0, 0, 0, 1 );
+         
+   //===============================================//
+   // Parallax occlusion mapping offset computation //
+   //===============================================//      
+   float fCurrHeight = 0.0;
+   float fStepSize   = 1.0 / (float) nNumSteps;
+   float fPrevHeight = 1.0;
+   float fNextHeight = 0.0;
+
+   int nStepIndex = 0;
+//   bool bCondition = true;
+   
+   float2 dx = ddx( inTexCoord );
+   float2 dy = ddy( inTexCoord );
+   
+   float2 vTexOffsetPerStep = fStepSize * vParallax;
+   
+   float2 vTexCurrentOffset = inTexCoord;
+   float fCurrentBound = 1.0;
+   
+   float x = 0;
+   float y = 0;
+   float xh = 0;
+   float yh = 0;   
+   
+   float2 texOffset2 = 0;
+   
+   bool bCondition = true;
+   while ( bCondition == true && nStepIndex < nNumSteps ) 
+   {
+      vTexCurrentOffset -= vTexOffsetPerStep;
+      
+      fCurrHeight = tex2Dgrad( HeightMapSampler, vTexCurrentOffset, dx, dy ).r;
+            
+      fCurrentBound -= fStepSize;
+      
+      if ( fCurrHeight > fCurrentBound ) 
+      {                
+         x  = fCurrentBound; 
+         y  = fCurrentBound + fStepSize; 
+         xh = fCurrHeight;
+         yh = fPrevHeight;
+         
+         texOffset2 = vTexCurrentOffset - vTexOffsetPerStep;
+                  
+         bCondition = false;
+      }
+      else
+      {
+         nStepIndex++;
+         fPrevHeight = fCurrHeight;
+      }
+     
+   }   // End of while ( bCondition == true && nStepIndex > -1 )#else
+
+   fCurrentBound -= fStepSize;
+   
+   float fParallaxAmount;
+   float numerator = (x * (y - yh) - y * (x - xh));
+   float denomenator = ((y - yh) - (x - xh));
+	// avoid NaN generation
+   if( ( numerator == 0.0f ) && ( denomenator == 0.0f ) )
+   {
+      fParallaxAmount = 0.0f;
+   }
+   else
+   {
+      fParallaxAmount = numerator / denomenator;
+   }
+
+   float2 vParallaxOffset = vParallax * (1 - fParallaxAmount );
+
+   // Sample the height at the next possible step:
+   fNextHeight = tex2Dgrad( HeightMapSampler, texOffset2, dx, dy ).r;
+   
+   // Original offset:
+   float2 texSampleBase = inTexCoord - vParallaxOffset;
+
+   return texSampleBase;
+
+#if 0
+   cResultColor.rgb = ComputeDiffuseColor( texSampleBase, vLight );
+        
+   float fBound = 1.0 - fStepSize * nStepIndex;
+   if ( fNextHeight < fCurrentBound )
+//    if( 0 )
+   {
+      //void DoIteration( in float2 vParallaxJittered, in float3 vLight, inout float4 cResultColor )
+      //cResultColor.rgb = float3(1,0,0);
+      DoIteration( vParallax + vPixelSize, vLight, fStepSize, inTexCoord, nStepIndex, dx, dy, fBound, cResultColor );
+      DoIteration( vParallax - vPixelSize, vLight, fStepSize, inTexCoord, nStepIndex, dx, dy, fBound, cResultColor );
+      DoIteration( vParallax + float2( -vPixelSize.x, vPixelSize.y ), vLight, fStepSize, inTexCoord, nStepIndex, dx, dy, fBound, cResultColor );
+      DoIteration( vParallax + float2( vPixelSize.x, -vPixelSize.y ), vLight, fStepSize, inTexCoord, nStepIndex, dx, dy, fBound, cResultColor );
+
+      cResultColor.rgb /= 5;
+//      cResultColor.rgb = float3( 1.0f, 0.0f, 0.0f );
+   }   // End of if ( fNextHeight < fCurrentBound )
+  
+#if DOSHADOWS
+   {
+      //============================================//
+      // Soft shadow and self-occlusion computation //
+      //============================================//
+      // Compute the blurry shadows (note that this computation takes into 
+      // account self-occlusion for shadow computation):
+      float sh0 =  tex2D( sNormalMap, texSampleBase).w;
+      float shA = (tex2D( sNormalMap, texSampleBase + inXY * 0.88 ).w - sh0 - 0.88 ) *  1 * fShadowSoftening;
+      float sh9 = (tex2D( sNormalMap, texSampleBase + inXY * 0.77 ).w - sh0 - 0.77 ) *  2 * fShadowSoftening;
+      float sh8 = (tex2D( sNormalMap, texSampleBase + inXY * 0.66 ).w - sh0 - 0.66 ) *  4 * fShadowSoftening;
+      float sh7 = (tex2D( sNormalMap, texSampleBase + inXY * 0.55 ).w - sh0 - 0.55 ) *  6 * fShadowSoftening;
+      float sh6 = (tex2D( sNormalMap, texSampleBase + inXY * 0.44 ).w - sh0 - 0.44 ) *  8 * fShadowSoftening;
+      float sh5 = (tex2D( sNormalMap, texSampleBase + inXY * 0.33 ).w - sh0 - 0.33 ) * 10 * fShadowSoftening;
+      float sh4 = (tex2D( sNormalMap, texSampleBase + inXY * 0.22 ).w - sh0 - 0.22 ) * 12 * fShadowSoftening;
+      
+      // Compute the actual shadow strength:
+      float fShadow = 1 - max( max( max( max( max( max( shA, sh9 ), sh8 ), sh7 ), sh6 ), sh5 ), sh4 );
+
+      cResultColor.rgb *= fShadow * 0.6 + 0.4;
+   }
+#endif
+   
+   return cResultColor;
+#endif
+}
+
 
 //======================================//
 // HSL Color space conversion routines  //
@@ -598,10 +755,15 @@ float3 TextureCombinePostLighting( float3 lit_baseColor, float4 detailColor, int
 	{
  		// fade in an unusual way - instead of fading out color, remap an increasing band of it from
  		// 0..1
-		if ( fBlendFactor > 0.5)
-			lit_baseColor += min(1, (1.0/fBlendFactor)*max(0, detailColor.rgb-(1-fBlendFactor) ) );
-		else
-			lit_baseColor += 2*fBlendFactor*2*max(0, detailColor.rgb-.5);
+		//if (fBlendFactor > 0.5)
+		//	lit_baseColor += min(1, (1.0/fBlendFactor)*max(0, detailColor.rgb-(1-fBlendFactor) ) );
+		//else
+		//	lit_baseColor += 2*fBlendFactor*2*max(0, detailColor.rgb-.5);
+
+		float f = fBlendFactor - 0.5;
+		float fMult = (f >= 0) ? 1.0/fBlendFactor : 4*fBlendFactor;
+		float fAdd = (f >= 0) ? 1.0-fMult : -0.5*fMult;
+		lit_baseColor += saturate(fMult * detailColor.rgb + fAdd);
 	}
 	return lit_baseColor;
 }
