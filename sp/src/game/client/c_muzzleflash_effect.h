c6a23b42d78edd431cdba7adfcfa739916c42569
Ivan Suvorov <avatar2329@gmail.com>
2020-09-08 12:22:23 +0600

    [THS] Add Muzzleflash effect.

---
+++ sp/src/game/client/c_muzzleflash_effect.h
@@ -0,0 +1,29 @@
+#ifndef C_MUZZLEFLASH_EFFECT_H
+#define C_MUZZLEFLASH_EFFECT_H
+
+
+class C_MuzzleflashEffect
+{
+public:
+
+	C_MuzzleflashEffect();
+	~C_MuzzleflashEffect();
+
+	virtual void UpdateLight(const Vector &vecPos, const Vector &vecDir, const Vector &vecRight, const Vector &vecUp,
+		float flStrength );
+
+	float GetHorizontalFOV() const { return m_flHorizontalFOV; }
+
+	ClientShadowHandle_t GetFlashlightHandle( void ) { return m_FlashlightHandle; }
+
+protected:
+
+	void UpdateLightNew(const Vector &vecPos, const Vector &vecDir, const Vector &vecRight, const Vector &vecUp);
+
+	ClientShadowHandle_t m_FlashlightHandle;
+	CTextureReference m_FlashlightTexture;
+
+	float m_flHorizontalFOV;
+};
+
+#endif
