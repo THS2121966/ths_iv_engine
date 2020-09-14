599d9284e5a078a2df94dd5c007359478e20c17f
THS2121966 <avatar2329@gmail.com>
2020-09-14 19:25:01 +0700

    [THS] Update new_water Shaders.

--- sp/src/materialsystem/stdshaders/buildsdkshaders.bat
+++ sp/src/materialsystem/stdshaders/buildsdkshaders.bat
@@ -29,7 +29,7 @@ set ARG_EXTRA=
 %BUILD_SHADER% stdshader_dx9_30			-game %GAMEDIR% -source %SOURCEDIR% -dx9_30	-force30
 
 %BUILD_SHADER% ths_posteffects		-game %GAMEDIR% -source %SOURCEDIR%
-rem %BUILD_SHADER% ths_new_water		-game %GAMEDIR% -source %SOURCEDIR%
+%BUILD_SHADER% ths_new_water		-game %GAMEDIR% -source %SOURCEDIR%
 
 rem echo.
 if not "%dynamic_shaders%" == "1" (
